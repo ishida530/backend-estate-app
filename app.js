c413c2e1a7995b27259d38cf669e0872b8d242c9
ishida530 <ishida530@gmail.com>
2024-05-29 15:19:34 +0200

    handle crud user

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -17,6 +19,7 @@ app.use(cors({
     credentials: true
 }));
 
+app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/test', testRoute);
