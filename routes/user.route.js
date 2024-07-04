13f9cdfba02be56924715be1f9f062c960efa098
ishida530 <ishida530@gmail.com>
2024-07-04 17:24:27 +0200

    in progress

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,22 @@
-import express from 'express';
-import { getUsers, getUser, updateUser, deleteUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../middleware/verifyToken.js';
+import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+  savePost,
+  profilePosts,
+  getNotificationNumber
+} from "../controllers/user.controller.js";
+import {verifyToken} from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get('/', getUsers);
-router.get('/:id', verifyToken, getUser);
-router.put('/:id', verifyToken, updateUser);
-router.delete('/:id', verifyToken, deleteUser);
+router.get("/", getUsers);
+router.put("/:id", verifyToken, updateUser);
+router.delete("/:id", verifyToken, deleteUser);
+router.post("/save", verifyToken, savePost);
+router.get("/profilePosts", verifyToken, profilePosts);
+router.get("/notification", verifyToken, getNotificationNumber);
 
 export default router;
